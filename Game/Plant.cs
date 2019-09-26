65cec6437d24c8984b5c12a906fe8114f10b7cea
Student <example@example.com>
2019-09-26 16:39:26 -0700

    update

--- Game/Plant.cs
+++ Game/Plant.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace Garden
 {
@@ -19,9 +20,14 @@ namespace Garden
     }
     class Plant
     {
-        private int[] _NewGrowth = {24,35};
-        public static int Height = 30;
-        public static int Width = 70;
+        private int[] _Seed = {(3*(Height/4)) +2,Width/2};
+        List<int []> growthList = new List<int []> {};
+        public int nutrientLevel { get; set; }
+        public int moistureLevel { get; set; }
+        public int energyLevel { get; set; }
+        private int branchTimer = 0;
+        public static int Height = 60;
+        public static int Width = 60;
         private int _ySun = 0;
         private int _xSun = 0;
         private char[,] _Map;
@@ -30,18 +36,22 @@ namespace Garden
         private const char _NightSky = (char)9617;
         private const char _Soil = (char)9619;
         private const char _Stock = (char)9580;
-        private const char _Seed = ',';
+        private const char _SeedChar = (char)8859;
 
         public Plant()
         {
-            // Width = 70;
-            // Height = 30;
+            nutrientLevel = 10;
+            moistureLevel = 10;
+            energyLevel = 10;
+            
+
+            growthList.Add(_Seed);
             char[,] newMap = new char[Height,Width];
             for (int i=0; i<Height; i++)
             {
                 //if height is less than 1/4
                 if(i <= 3*(Height / 4)) 
-                { 
+                {
                     //make
                     for(int j=0;j<Width - 2; j++)
                     {
@@ -58,9 +68,31 @@ namespace Garden
                     newMap[i,Width - 1] = '\n';
                 }
             }
+            newMap[(3*(Height/4)) +2,Width/2] = _SeedChar;
+            
             _Map = newMap;
 
         }
+
+        public void PlantLevels()
+        {
+            energyLevel--;
+            moistureLevel--;
+            nutrientLevel--;
+            Draw();
+
+
+        }
+
+        public void GameOver()
+        {
+            if (energyLevel == 0 || moistureLevel == 0 || nutrientLevel == 0)
+            {
+                Console.WriteLine("Game Over!");
+                System.Environment.Exit(1);                
+            }
+        }
+
 
         public void Action(char input, int ySun, int xSun)
         {
@@ -68,13 +100,23 @@ namespace Garden
             _ySun = ySun;
             _xSun = xSun;
             //do the action
+            if (input == 'w')
+            {
+                moistureLevel++;
+                energyLevel++;
+
+            }
+            else if (input == 'n')
+            {
+                nutrientLevel++;
+                energyLevel++;
+            }
 
             //update plant
 
             //check water
                 //if water is low
                     //leaves lose health
-
             //check nutrients
                 //update unhealthy leaves to healthy if nutritients are sufficient
                 //if it is good be able to grow a leaf
@@ -93,31 +135,62 @@ namespace Garden
 
         private void UpdatePlant()
         {
-            GrowBranches();
-            DrawPlant();
+            branchTimer++;
+            int length = growthList.Count;
+            for (int i=0; i < length; i++)
+            {
+                growthList[i] = GrowBranches(growthList[i]);
+            }
+            Draw();
             return;
         }
 
-        private void GrowBranches()
+        private int[] GrowBranches(int [] branch)
         {
+            Random rand = new Random();
             //figure out how far to grow
                 int growStocks = 1;
             //figure out what direction to grow
                 //grow relative to moving sun
-                int xDirection = 0;
+                int xDirection = rand.Next(-1,2);
             //loop through and place stock characters
             for (int i=0; i<growStocks; i++)
             {
-                int [] newPos = {_NewGrowth[0]-1, _NewGrowth[1]-xDirection};
+                int [] newPos = {branch[0]-1, branch[1]-(2*xDirection)};
                 if(InBounds(newPos))
                 {
-                    //place a new piece
-                    _Map[_NewGrowth[0]-1, _NewGrowth[1]-xDirection] = _Stock;
+                    if (xDirection > 0) 
+                    {
+
+                    _Map[newPos[0], newPos[1]+1] = _Stock;
+
+                    }
+                    else if (xDirection < 0)
+                    {
+                    _Map[newPos[0], newPos[1]-1] = _Stock;
+                    }
+                    _Map[newPos[0], newPos[1]] = _Stock;
                     //update newgrowth
-                    _NewGrowth = newPos;
-                }
+                    branch = newPos;
+                    if(branchTimer % 6 == 0)
+                    {
+                        int[] newLeft = {newPos[0],newPos[1]};
+                        int[] newRight = {newPos[0],newPos[1]+1};
+                        if (InBounds(newLeft))
+                        {
+                            // growthList.Add(newLeft);
+                        }
+                        // if (InBounds(newRight))
+                        // {
+                        //     growthList.Add(newRight);
+                        // }
+                    }
+                        
+                } 
             }
+            return branch;
         }
+
         private bool InBounds(int [] newPos)
         {
             bool inBounds = false; 
@@ -130,9 +203,9 @@ namespace Garden
 
         }
 
-        private void DrawPlant()
+        private void Draw()
         {
-            Thread.Sleep(5);
+            Thread.Sleep(20);
             Console.Clear();
             string mapString = "\r";
             _Map[_ySun,_xSun] = _Sun;
@@ -144,6 +217,15 @@ namespace Garden
                     mapString += _Map[i,j];
                 } 
             }
+            for(int i=0; i<Width - 2; i++)
+            {
+                mapString += (char)9552;
+            }
+            mapString += '\n';
+            mapString += "Nutrient Level: " + nutrientLevel + '\n';
+            mapString += "Soil Moisture Level: " + moistureLevel + '\n';
+            mapString += "Energy Level: " + energyLevel + '\n';
+
             Console.WriteLine(mapString);
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,27 +1,49 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
-
+using System.Threading;
 
 namespace Garden 
 {
     class Program
     {
-    static void Main()
-    {
-        Sun sun = new Sun();
-        Plant plant = new Plant();
-        // Plant.Show;
-        ConsoleKeyInfo cki = Console.ReadKey(true);
-        while (cki.KeyChar != 'q')
+        static void Main()
         {
-            cki = Console.ReadKey(true);
-            if(!Console.KeyAvailable)
-            {
-                plant.Action(cki.KeyChar, sun.YSun, sun.XSun);
-            }
+            Sun sun = new Sun();
+            Plant plant = new Plant();
+            // Plant.Show;
+            GameDriver(sun, plant);
+
         }
 
-    // program code goes here
+        public static void GameDriver(Sun sun, Plant plant)
+        {
+            
+            while (!Console.KeyAvailable)
+            {
+                Thread.Sleep(1000);
+                plant.PlantLevels();
+                plant.GameOver();
+                plant.Action(' ', sun.YSun, sun.XSun);
+                GameDriver(sun,plant);           
+            }
+            //ConsoleKeyInfo cki = Console.ReadKey(true);
+            // Console.WriteLine((char)Console.In.Peek());
+            if (Console.In.Peek() != -1)
+            {
+                char cki = (char)Console.Read();
+                FlushKeyboard();
+                plant.Action(cki, sun.YSun, sun.XSun);
+            }
+            Thread.Sleep(1000);
+            GameDriver(sun,plant);
+        }
+
+        private static void FlushKeyboard()
+        {
+            while (Console.In.Peek() != -1)
+            Console.In.Read();
+         }
     }
-    }
+    
 }
